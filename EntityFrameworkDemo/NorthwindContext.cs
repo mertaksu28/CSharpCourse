ce74f97fc78717f2a0d23c4c3bb356089dac934f
mertaksu28 <mertaksu28@outlook.com.tr>
2021-09-15 17:44:25 +0300

    northwind ile bağlantı kurma

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    //Context classı dosyası; bizim veritabanı ile uygulamamızdaki nesneler arasındaki ilişkiyi kurduğumuz yerdir.
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Database=Northwind; Trusted_Connection=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
