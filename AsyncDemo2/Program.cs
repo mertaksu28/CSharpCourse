a34f2464b3b34c256c61e42c42f3c7a1ecfeb93c
mertaksu28 <mertaksu28@outlook.com.tr>
2021-11-24 20:16:13 +0300

    async and await

---
+++ AsyncDemo2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread başladı.{Thread.CurrentThread.ManagedThreadId}");
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process1Async();
+            Process2Async();
+            Process1();
+            Process2();
+        }
+
+        static void Process1()
+        {
+            Console.WriteLine($"Senkron İşlem 1 başladı.{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process1Async()
+        {
+            await Task.Run(() =>
+             {
+                 Console.WriteLine($"Async İşlem 1 başladı.{Thread.CurrentThread.ManagedThreadId}");
+             });
+        }
+
+        static void Process2()
+        {
+            Console.WriteLine($"Senkron İşlem 2 başladı.{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process2Async()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Async İşlem 2 başladı.{Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+
+    }
+}
